cbd100954d55a270e9d0d74fa6645f497ee30349
Pedro Fontes <pedro7fontes@gmail.com>
2023-01-31 21:14:56 -0300

    fix: removed unused import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Style from "./Navbar.module.scss";
 import Toggler from "./home/Toggler";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Box } from "@mui/material";
 import { info } from "../info/Info";
 
